15703586e7d14043971977704a59d63113ec954a
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-16 21:45:21 -0500

    Added basic commands.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Add code
+
+option = input("settings>>> ")
+if option == "reset":
+  print("Feature still a WIP.")
+elif option == "change-name":
+  user = input("settings/change-name>>> ")
+  print("Name changed.")
+elif option == "clear":
+  print("Are you sure?")
+  option = input("(y/n)>>> ")
+  if option == "y":
+    os.system('clear')
+    print("Cleared.")
+    time.sleep(5)
+    os.system('clear')
+  elif option == "n":
+    print("Canceled operation.")
+  else:
+    print("Invalid Input, Canceling...")
